634ede76d4edae5d500dfb005b4b58a7aafe1bb4
Uzoma Stephen Uzoma <usumg1165@gmail.com>
2023-01-04 21:03:13 +0100

    Create Node class and Tree class with buildTree(), bst() and prettyPrint()

--- script.js
+++ script.js
@@ -2,3 +2,54 @@ console.log('uzomian...');
 
 const log = (...a) => console.log(...a);
 
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Tree {
+    constructor (arr) {
+        this.arr = arr;
+        this.root = this.buildTree();
+    }
+
+    buildTree (arr = this.arr) {
+        // Sort the array and remove duplicates
+        const modArr = [...new Set(arr)].sort((a, b) => a - b);
+
+        return this.bst(modArr);
+    }
+
+    bst (arr, start = 0, end = arr.length - 1) {
+        // Get the middle of the array;
+        const mid = ~~((start + end) / 2);
+
+        // Base case
+        if (start > end) return null;
+
+        // Create the balanced tree
+        const root = new Node(arr[mid]);
+        root.left = this.bst(arr, start, mid - 1);
+        root.right = this.bst(arr, mid + 1, end);
+
+        return root;
+    }
+
+    prettyPrint (node = this.root, prefix = '', isLeft = true) {
+        if (node.right !== null) {
+            this.prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+          }
+          console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+          if (node.left !== null) {
+            this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+          }
+    }
+}
+
+const tree = new Tree([1, 2, 3, 4, 5]);
+log(tree.root);
+
+tree.prettyPrint();
